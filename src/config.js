c0fbebb84108323a966d4dda2bfe7495ac990928
jkiemele <jkiemele@gmail.com>
2026-05-23 07:45:28 -0700

    Update config.js
    Added in link to for responses to Google Sheet

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 window.CLEARPATH_CONFIG = {
-  sheetsEndpoint: ""
+  sheetsEndpoint: "https://script.google.com/macros/s/AKfycbxVJi8qAdGhHwlI5uAjGmPxrwNBvq-_tJeG49H2bmClg76dOdk_0-ysB6WS38Wp1OCc/exec"
 };
